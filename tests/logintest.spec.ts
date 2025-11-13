aeb55e1d46112c5f290206292ee24ba7aebc611b
Deependra Bohara <52132862+Deependra17@users.noreply.github.com>
2025-11-14 01:56:06 +0545

    Fix typo in username for login test

--- tests/logintest.spec.ts
+++ tests/logintest.spec.ts
@@ -5,7 +5,7 @@ import {WaitClass} from '../utils/waitClass';
 test('login should succeed with valid credentials', async ({page}) => {
     const loginPage = new LoginPage(page);
     await page.goto('https://UAT.sumx.app/');
-    await loginPage.enterUsername('ektest@yopmail.com');
+    await loginPage.enterUsername('@test@yopmail.com');
     await loginPage.enterPassword('Tester@123');
 
     await WaitClass.waitForEnabled(loginPage.loginButton);
